8e99dbf78ef7e0bc7030ef83b75206af7a01df17
Henry Lopes Pereira <73833452+HenrylPereira@users.noreply.github.com>
2023-12-03 03:18:18 +0000

    ajustes update

--- matricula/src/main/java/br/edu/univille/microservmatricula/controller/CursoAPIController.java
+++ matricula/src/main/java/br/edu/univille/microservmatricula/controller/CursoAPIController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,7 @@ public class CursoAPIController {
             new ResponseEntity<List<Curso>>
             (listacursos, HttpStatus.OK);
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<Curso> buscarCurso(@PathVariable("id")  String id){
         var curso = service.getById(id);
@@ -42,6 +44,21 @@ public class CursoAPIController {
             new ResponseEntity<Curso>
             (curso, HttpStatus.OK);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Curso> atualizarCurso(@PathVariable("id")  String id, @RequestBody Curso curso){
+        if(curso == null || id == "" || id == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        curso = service.update(id, curso);
+        if(curso == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return 
+            new ResponseEntity<Curso>
+            (curso, HttpStatus.OK);
+    }
+
     @PostMapping
     public ResponseEntity<Curso> inserirCurso(@RequestBody Curso curso){
         if(curso == null){
--- matricula/src/main/java/br/edu/univille/microservmatricula/controller/MatriculaAPIController.java
+++ matricula/src/main/java/br/edu/univille/microservmatricula/controller/MatriculaAPIController.java
@@ -4,6 +4,7 @@ package br.edu.univille.microservmatricula.controller;
 
 import java.util.List;
 
+import br.edu.univille.microservmatricula.entity.Curso;
 import br.edu.univille.microservmatricula.entity.Matricula;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +33,7 @@ public class MatriculaAPIController {
             new ResponseEntity<List<Matricula>>
             (listaMatriculas, HttpStatus.OK);
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<Matricula> buscarMatricula(@PathVariable("id")  String id){
         var matricula = service.getById(id);
@@ -41,6 +44,7 @@ public class MatriculaAPIController {
             new ResponseEntity<Matricula>
             (matricula, HttpStatus.OK);
     }
+
     @PostMapping
     public ResponseEntity<Matricula> inserirMatricula(@RequestBody Matricula matricula){
         if(matricula == null){
@@ -51,6 +55,21 @@ public class MatriculaAPIController {
             new ResponseEntity<Matricula>
             (matricula, HttpStatus.OK);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Matricula> atualizarMatricula(@PathVariable("id")  String id, @RequestBody Matricula matricula){
+        if(matricula == null || id == "" || id == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        matricula = service.update(id, matricula);
+        if(matricula == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return 
+            new ResponseEntity<Matricula>
+            (matricula, HttpStatus.OK);
+    }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Matricula> removerMatricula(@PathVariable("id")  String id){
         if(id == ""  || id == null){
